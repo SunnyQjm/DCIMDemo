99d3b93810526688dca5eeae6a2d6e68f7a5709d
MingJ <qjm253@gmail>
2018-05-14 10:06:21 +0800

    demo效果图

--- dcim/src/main/java/com/j/ming/dcim/model/params/EasyBarParams.kt
+++ dcim/src/main/java/com/j/ming/dcim/model/params/EasyBarParams.kt
@@ -5,8 +5,11 @@ import android.support.annotation.DrawableRes
 import android.support.annotation.StringRes
 import com.j.ming.dcim.R
 
-class EasyBarParams(val title: String = "", @DrawableRes val leftRes: Int = R.drawable.back,
-                    @DrawableRes val rightRes: Int = R.drawable.icon_sure, val leftText: String = "",
-                    val rightText: String = "", @StringRes val titleRes: Int = -1,
+class EasyBarParams(val title: String = "",
+                    @DrawableRes val leftRes: Int = R.drawable.back,
+                    @DrawableRes val rightRes: Int = R.drawable.icon_sure,
+                    val leftText: String = "",
+                    val rightText: String = "",
+                    @StringRes val titleRes: Int = -1,
                     @ColorRes val barBgColor: Int = R.color.colorPrimary){
 }
